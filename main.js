0d689123148e802286a78c1bc5a275cb6ed3dcce
luiover <157648225+luiover@users.noreply.github.com>
2026-04-07 13:35:55 +0200

    Rearrange structures & return Number type
    Reorganize code by avoiding deep nesting and optimize to avoid
    repeated code for future reference. Separate functions with commented
    sections. Improve float output handling to reduce unnecessary trailing zeros.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const decimalLimit = 6; // Limit the number of decimals
 const errorText = 'Error 404 ☠️🪦';
-let clearAfterResult = true;
+let clearAfterResult = false;
 let tempResult = true;
 
 let num1 = '',
@@ -16,9 +16,79 @@ const boop = document.querySelector('#boop');
 num1 = display.textContent;
 console.log(num1);
 
-boop.addEventListener('click', (e) => alert('Beep! 🚨'));
+// // // // // // // // // // // // // // // // // //
+// Assign button click logic to each calculator button
+for (const button of buttons) {
+	button.addEventListener('click', buttonClicked);
+}
 
-toggleClear.addEventListener('click', (e) => {
+// Extra buttons click event logic
+boop.addEventListener('click', (e) => alert('Beep! 🚨'));
+toggleClear.addEventListener('click', toggleClearFn);
+// // // // // // // // // // // // // // // // // //
+
+function buttonClicked(e) {
+	const btnElement = e.target.closest('.btn');
+	const value = btnElement.dataset.value;
+
+	if (tempResult) {
+		clearDisplay();
+		tempResult = false;
+	}
+
+	switch (value) {
+		case 'clear':
+			clearDisplay();
+			break;
+
+		case 'delete':
+			cancelLast();
+			break;
+
+		case '=':
+			// Ignore if the expression is incorrect
+			if (!num2 || !operator) {
+				abort();
+				break;
+			}
+
+			calculate();
+			break;
+
+		default:
+			// Update numbers if the button is a number
+			if (!isNaN(value) || value === '.') {
+				updateNumVariable(value);
+				break;
+			}
+
+			// Handle early operator button
+			if (!num1) {
+				handleStartingSymbol(value);
+				break;
+			}
+
+			// Check if an operator is already typed
+			if (operator) {
+				// Handle case where an expression is already typed
+				if (num2) {
+					// Calculate before adding another operator
+					calculate();
+				} else {
+					// Remove the old unwanted operator
+					display.textContent = display.textContent.slice(0, -1);
+				}
+			}
+
+			// Add the operator
+			operator = value;
+			display.textContent += value;
+	}
+	// Debugging purposes
+	console.log('Num1:', num1, 'Op:', operator, 'Num2:', num2);
+}
+
+function toggleClearFn(e) {
 	const btnClass = [...e.target.classList];
 	if (btnClass.some((item) => item === 'active')) {
 		// Disable clearAfterResult
@@ -31,77 +101,46 @@ toggleClear.addEventListener('click', (e) => {
 		e.target.classList.add('active');
 		clearAfterResult = true;
 	}
-	console.log(btnClass);
 	e.target;
-});
+}
 
-// Handle calculator button presses
-for (const button of buttons) {
-	button.addEventListener('click', (e) => {
-		const btnElement = e.target.closest('.btn');
-		const value = btnElement.dataset.value;
+// Input handling and formatting helpers
+function handleStartingSymbol(value) {
+	// Consider positive or negative values
+	if (value === '-' || value === '+') {
+		num1 += value;
+		display.textContent += value;
+	} else {
+		abort();
+	}
+}
 
-		if (tempResult) {
-			clearDisplay();
-			tempResult = false;
-		}
+function limitDecimals(number) {
+	// Count the number of decimals
+	const decimal = String(number)?.split('.')[1];
+	if (decimal && decimal.length > decimalLimit) {
+		const shorterNum = Number(number).toFixed(decimalLimit);
+		return Number(shorterNum);
+	} else {
+		return number;
+	}
+}
 
-		switch (value) {
-			case 'clear':
-				clearDisplay();
-				break;
+// Display-related mutating functions
+function clearDisplay() {
+	display.textContent = '';
+	clearVariables();
+}
 
-			case 'delete':
-				cancelLast();
-				break;
-
-			case '=':
-				if (!num2 || !operator) {
-					abort();
-					break;
-				}
-				calculate();
-				break;
-
-			default:
-				// Check if the button is an operator
-				if (isNaN(value)) {
-					// If no num1 yet, treat + or - as number part, else it's a mistake
-					if (!num1) {
-						if (value === '-' || value === '+') {
-							num1 += value;
-							display.textContent += value;
-						} else {
-							abort();
-						}
-						break;
-					}
-
-					// If the operator is already assigned, replace it
-					if (operator) {
-						if (num2) {
-							// Write the result and append the operator
-							calculate();
-							display.textContent += value;
-							// Set the second operator to be the current
-							operator = value;
-							break;
-						}
-						display.textContent = display.textContent.slice(0, -1);
-					}
-					operator = value;
-				} else {
-					// Update num1 or num2 based on whether an operator is assigned
-					if (!operator) {
-						num1 += value;
-					} else {
-						num2 += value;
-					}
-				}
-				display.textContent += value;
-		}
-		console.log('Num1:', num1, 'Op:', operator, 'Num2:', num2);
-	});
+function cancelLast() {
+	lastChar = display.textContent.slice(-1);
+	display.textContent = display.textContent.slice(0, -1);
+	// Check if an operator or number has been removed to update it
+	if (isNaN(lastChar)) {
+		operator = '';
+	} else {
+		num1 = String(num1).slice(0, -1);
+	}
 }
 
 function abort() {
@@ -129,32 +168,15 @@ function calculate() {
 	display.textContent = num1;
 }
 
-function cancelLast() {
-	lastChar = display.textContent.slice(-1);
-	display.textContent = display.textContent.slice(0, -1);
-	// Check if an operator or number has been removed to update it
-	if (isNaN(lastChar)) {
-		operator = '';
+// Handling variables functions
+function updateNumVariable(value) {
+	// Update num1 or num2 based on whether an operator is assigned
+	if (!operator) {
+		num1 += value;
 	} else {
-		num1 = String(num1).slice(0, -1);
+		num2 += value;
 	}
-}
-
-function limitDecimals(number) {
-	// Count the number of decimals
-	const decimal = String(number)?.split('.')[1];
-	if (decimal && decimal.length > decimalLimit) {
-		const shorterNum = Number(number).toFixed(decimalLimit);
-		return shorterNum;
-		console.log(shorterNum);
-	} else {
-		return number;
-	}
-}
-
-function clearDisplay() {
-	display.textContent = '';
-	clearVariables();
+	display.textContent += value;
 }
 
 function clearVariables() {
@@ -162,6 +184,8 @@ function clearVariables() {
 	num2 = '';
 	operator = '';
 }
+
+// Mathematical operations
 
 function operate(num1, operator, num2) {
 	switch (operator) {
